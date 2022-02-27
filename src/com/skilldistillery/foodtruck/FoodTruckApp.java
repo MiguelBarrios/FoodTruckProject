51bbcd59141f7a06eb8b8f2eb6b60ba162950d78
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-02-26 20:31:49 -0700

    User story #4 complete

--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -3,65 +3,80 @@ package com.skilldistillery.foodtruck;
 import java.util.Scanner;
 
 public class FoodTruckApp {
-	
+
 	private final int MAX_NUM_TRUCKS = 5;
-	
+
 	private FoodTruck[] trucks = new FoodTruck[MAX_NUM_TRUCKS];
-	
+
 	private int numberOfTrucks = 0;
-	
+
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
 		FoodTruckApp app = new FoodTruckApp();
 		
 		app.getTruckDataFromUser(scanner);
-		app.displayMenu();
-		int userSelection = app.getNumericInput(scanner);
-		System.out.println(userSelection);
-		app.displayAverageFoodTruckRating();
-		app.displayHighestRatedTruck();
 		
-		//app.displayTrucks();
-		
+		int userSelection;
+		do{
+			app.displayMenu();
+			userSelection = app.getNumericInput(scanner);
+			
+			if(userSelection == 1) {
+				app.listFoodTrucks();
+			}
+			else if(userSelection == 2) {
+				app.displayAverageFoodTruckRating();
+			}
+			else if(userSelection == 3) {
+				app.displayHighestRatedTruck();
+			}
+			else if(userSelection == 4) {
+				System.out.println("GoodBye!");
+				break;
+			}
+			else {
+				System.out.println("Invalid selction");
+			}
+			
+		}while(userSelection != 4);
 
 	}
-	
+
 	public void displayHighestRatedTruck() {
 		int highestRating = 0;
 		// Find highest Rating
-		for(int i = 0; i < this.numberOfTrucks; ++i) {
-			if(this.trucks[i].getRating() > highestRating) {
+		for (int i = 0; i < this.numberOfTrucks; ++i) {
+			if (this.trucks[i].getRating() > highestRating) {
 				highestRating = this.trucks[i].getRating();
 			}
 		}
-		
+
 		// show trucks with highest rating
 		System.out.println("Highest Rated Trucks");
-		for(int i = 0 ; i< this.numberOfTrucks; ++i) {
-			if(trucks[i].getRating() == highestRating) {
+		for (int i = 0; i < this.numberOfTrucks; ++i) {
+			if (trucks[i].getRating() == highestRating) {
 				System.out.println(trucks[i]);
 			}
 		}
-		
-		
+
 	}
-	
+
 	public void listFoodTrucks() {
-		for(int i = 0; i < this.numberOfTrucks; ++i) {
+		for (int i = 0; i < this.numberOfTrucks; ++i) {
 			System.out.println(trucks[i]);
 		}
 	}
-	
+
 	public void displayAverageFoodTruckRating() {
 		double sum = 0;
-		for(int i = 0; i < this.numberOfTrucks; ++i) {
+		for (int i = 0; i < this.numberOfTrucks; ++i) {
 			sum += this.trucks[i].getRating();
 		}
 		double average = sum / this.numberOfTrucks;
 		System.out.println("Average Rating: " + average);
 	}
-	
+
 	public void displayMenu() {
 		System.out.println("Select from the following");
 		System.out.println("1) List all existing food trucks");
@@ -69,30 +84,28 @@ public class FoodTruckApp {
 		System.out.println("3) Show highest-rated food truck");
 		System.out.println("4) Quit");
 	}
-	
-	
+
 	public int getNumericInput(Scanner scanner) {
 		System.out.print("Selection: ");
 		int input = scanner.nextInt();
 		return input;
 	}
 
-	
 	public void getTruckDataFromUser(Scanner scanner) {
-		for(int i = 0; i < MAX_NUM_TRUCKS; ++i) {
+		for (int i = 0; i < MAX_NUM_TRUCKS; ++i) {
 			FoodTruck foodTruck = getFoodTruckInfo(scanner);
-			if(foodTruck == null)
+			if (foodTruck == null)
 				break;
-			
+
 			this.trucks[numberOfTrucks] = foodTruck;
 			++numberOfTrucks;
 		}
 	}
-	
+
 	public FoodTruck getFoodTruckInfo(Scanner scanner) {
 		System.out.print("Enter name of truck or quit to exit: ");
 		String name = scanner.nextLine();
-		if(name.equalsIgnoreCase("Quit")){
+		if (name.equalsIgnoreCase("Quit")) {
 			return null;
 		}
 		System.out.print("Enter food type: ");
@@ -100,12 +113,12 @@ public class FoodTruckApp {
 		System.out.print("Enter rating: ");
 		int rating = scanner.nextInt();
 		scanner.nextLine();
-		
+
 		return new FoodTruck(name, type, rating);
 	}
-	
+
 	public void displayTrucks() {
-		for(int i = 0; i < this.numberOfTrucks; ++i) {
+		for (int i = 0; i < this.numberOfTrucks; ++i) {
 			System.out.println(this.trucks[i]);
 		}
 	}
